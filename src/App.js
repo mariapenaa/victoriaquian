436ec4f3e790fcf1fbf44985c2945916b6310eaf
Maria Peña <mpena@udesa.edu.ar>
2021-02-22 15:57:47 -0300

    Copia de pagina original

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import logo from './logo.svg';
+
 import './App.css';
+import {NavBar} from './components/Nav';
+import {PhotoGrid} from './components/Grid';
 
 function App() {
+
+  const hello = () =>{
+    alert("hey")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+    <div className="main-app">
+    <div className="main-app-wrap">
+      <h1>VICTORIA QUIAN</h1>
+      <div class="box" onClick={hello}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </div>
+    </div>
+    <div className="section-1">
+    <NavBar />
+      <h2>first section</h2>
+    </div>
+    <div className="section-2">
+      <h2>second section</h2>
+    </div>
+  </div>
   );
 }
 
---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Nav.css'
+
+
+export function NavBar() {
+    return (
+    <div className="nav-bar sticky">
+        <img className="vq-logo" src="" />
+        <ul className="nav-items">
+            <li>Rollo</li>
+            <li>Momentos</li>
+            <li>Arte</li>
+            <li>ME</li>
+        </ul>
+        
+    </div>
+    );
+}
