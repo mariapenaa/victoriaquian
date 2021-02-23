6029991b4231064c1f88b059e62ddd0b4c9237d1
Maria Peña <mpena@udesa.edu.ar>
2021-02-23 12:38:49 -0300

    grid

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,25 +1,72 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
+import Image from 'react-bootstrap/Image';
 import ReactDOM from 'react-dom';
 import './Grid.css';
+import img1 from '../img/bellahadid.jpg';
+import img2 from '../img/fotoauto.jpeg';
+import img3 from '../img/IMG_9235.jpg';
+import img4 from '../img/IMG_9237.jpg';
+import img5 from '../img/IMG_9238.jpg';
+import img6 from '../img/IMG_9239.jpg';
+import img7 from '../img/IMG_9240.jpg';
+import img8 from '../img/IMG_9241.jpg';
+import img9 from '../img/IMG_9242.jpg';
+import img10 from '../img/IMG_9243.jpg';
+
+
 
 export function PhotoGrid() {
-    var fv = {
-        uno:"IMG_9235",
-    };
-
     return (
-    <Container fluid className="main-container-grid">
-    <Row>
-        <Col>
-            <Row>
-                <Col></Col>
-                <Col>HEY</Col>
-            </Row>
-            <Row>HEY</Row>
-        </Col>
-        <Col>HEY</Col>
-    </Row>
-    </Container>
+        <div class="row">
+            <div class="column">
+                <img src={img2} />
+                <img src={img7} />
+                <img src={img4} />
+                <img src={img1} />
+                <img src={img5} />
+                <img src={img6} />
+                <img src={img10} />
+                <img src={img8} />
+                <img src={img9} />
+                <img src={img3} />
+            </div>
+            <div class="column">
+                <img src={img3} />
+                <img src={img1} />
+                <img src={img2} />
+                <img src={img9} />
+                <img src={img5} />
+                <img src={img6} />
+                <img src={img7} />
+                <img src={img10} />
+                <img src={img4} />
+                <img src={img8} />
+            </div>
+            <div class="column">
+                <img src={img3} />
+                <img src={img4} />
+                <img src={img8} />
+                <img src={img5} />
+                <img src={img2} />
+                <img src={img1} />
+                <img src={img10} />
+                <img src={img6} />
+                <img src={img7} />
+                <img src={img9} />
+            </div>
+            <div class="column">
+                <img src={img4} />
+                <img src={img5} />
+                <img src={img1} />
+                <img src={img2} />
+                <img src={img10} />
+                <img src={img6} />
+                <img src={img7} />
+                <img src={img8} />
+                <img src={img3} />
+                <img src={img9} />
+            </div>
+        </div>
     );
 }
---
+++ src/components/Images.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+export function ImagesForGrid() {
+
+    return (
+       <h2>Hey</h2>
+    );
+}
