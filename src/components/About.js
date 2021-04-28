ff76afa2353e5eff24571cac879e198565b91b95
mariapenaa <mpena@udesa.edu.ar>
2021-04-27 22:42:13 -0300

    pagina about me

--- src/components/About.js
+++ src/components/About.js
@@ -2,14 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './stylesheet/About.css';
 import './stylesheet/main.css';
+import fotoVicky from "../img/IMG_9239.jpg"
+
 
 export function About() {
-    return (
-       <div className="about-wrapper">
-           <div className="about-me">
-               <p>Hola mi nombre es Vicky</p>
-           </div>
 
-       </div>
+
+
+    return (
+        <div className="container">
+            <div className="about-wrapper">
+                <div className="about-me">
+                    <h1>Soy Victoria Quian</h1>
+                    <p>Lorem Ipsum is simply dummy text of the printing and t
+                        ypesetting industry. Lorem Ipsum has been the industry's 
+                        standard dummy text ever since the 1500s, when an unknown 
+                        printer took a galley of type and scrambled it to 
+                        make a type specimen book. It has survived not only f
+                        ive centuries, but also the leap into electronic typesetting, 
+                        remaining essentially unchanged. It was popularised in the 1
+                        960s with the release of Letraset sheets containing Lorem 
+                        Ipsum passages, and more recently with desktop publishing 
+                        software like Aldus PageMaker including versions of Lorem Ipsum</p>
+                </div>
+                <img src={fotoVicky} className="foto-vicky" />
+            </div>
+        </div>
     );
 }
---
+++ src/photos.js
@@ -0,0 +1,68 @@
+import img1 from "./img/bellahadid.jpg"
+import img2 from "./img/fotoauto.jpeg"
+
+export const rutasFotos = [
+    {
+        id:0,
+        nobre: "rollo",
+        fotos: [
+            {
+                "id":0,
+                "alt":"Foto de bella hadid",
+                "fecha":"hoy",
+                "img":img1
+            },
+            {
+                "id":1,
+                "alt":"Foto de bella hadid",
+                "fecha":"hoy",
+                "img":require('./img/IMG_9235.jpg')
+            },
+            {
+                "id":2,
+                "alt":"Foto de bella hadid",
+                "fecha":"hoy",
+                "img":require('./img/IMG_9244.jpg')
+            },
+            {
+                "id":3,
+                "alt":"Foto de bella hadid",
+                "fecha":"hoy",
+                "img":require('./img/IMG_9244.jpg')
+            }
+        ]
+    }
+]
+
+export const rutasRollo = [
+    {
+        id:0,
+        nobre: "rollo",
+        fotos: [
+            {
+                "id":0,
+                "alt":"Foto de bella hadid",
+                "fecha":"hoy",
+                "img":img2
+            },
+            {
+                "id":1,
+                "alt":"Foto de bella hadid",
+                "fecha":"hoy",
+                "img":require('./img/IMG_9235.jpg')
+            },
+            {
+                "id":2,
+                "alt":"Foto de bella hadid",
+                "fecha":"hoy",
+                "img":require('./img/IMG_9244.jpg')
+            },
+            {
+                "id":3,
+                "alt":"Foto de bella hadid",
+                "fecha":"hoy",
+                "img":require('./img/IMG_9244.jpg')
+            }
+        ]
+    }
+]
