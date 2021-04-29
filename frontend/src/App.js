e130bd7f67fd1c8b90bc774af6314a7f4ded686a
mariapenaa <mpena@udesa.edu.ar>
2021-04-29 00:00:24 -0300

    cree backend y  cree contact

---
+++ frontend/src/App.js
@@ -0,0 +1,41 @@
+
+import './App.css';
+import {NavBar} from './components/Nav';
+import {Footer} from './components/Footer';
+import{Proyectos} from './components/Proyectos';
+import {Home} from './components/Home';
+import {About} from './components/About';
+import {Coleccion} from './components/Coleccion';
+import {ContactForm} from './components/ContactForm';
+
+
+
+
+//routes
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+
+function App() {
+
+ 
+
+  return (
+   <Router>
+      <Switch>
+        <div className="App">
+          {/* <Route path="/" exact component={Home} /> */}
+          <NavBar />
+            <Route path="/" exact component={Home} />
+            <Route path="/proyectos" exact component={Proyectos} />
+            <Route path="/about" exact component={About} />
+            <Route path="/proyectos/coleccion" exact component={Coleccion} />
+            <Route path="/contacto" exact component={ContactForm} />
+          <Footer />
+        </div >
+      </Switch>
+    </Router> 
+  );
+}
+
+
+export default App;
---
+++ frontend/src/components/ContactForm.js
@@ -0,0 +1,101 @@
+
+import React, { Component } from "react";
+import axios from "axios";
+import ReactDOM from 'react-dom';
+import './stylesheet/About.css';
+
+ export class ContactForm extends Component {
+    constructor() {
+        super();
+        this.state = {
+            name: "",
+            email: "",
+            message: "",
+            status: "Submit"
+        }; 
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+        
+        handleChange(event) {
+            const field = event.target.id;
+            if (field === "name") {
+                this.setState({ name: event.target.value });
+            } else if (field === "email") {
+                this.setState({ email: event.target.value });
+            } else if (field === "message") {
+                this.setState({ message: event.target.value });
+            }
+        }
+
+        handleSubmit(event) {
+            event.preventDefault();  
+            this.setState({ status: "Sending" });  
+            axios({
+                method: "POST",
+                url: "http://localhost:5000/contact",
+                data: this.state,
+                }).then((response) => {
+                    if (response.data.status === "sent") {
+                        alert("Message Sent");
+                        this.setState({ name: "", email: "", message: "", status: "Submit" });
+                    } else if (response.data.status === "failed") {
+                        alert("Message Failed");
+                    }
+                    });
+        }
+
+
+        render() {
+
+            let buttonText = this.state.status;
+
+          
+
+            
+            return (      
+                <div className="container">
+                    <h1>Contacto</h1>
+                    <div className="form-container">
+                        <form /* onSubmit={this.handleSubmit.bind(this)} */ className="form" method="POST">
+                            <div>
+                                <label htmlFor="name">Name:</label><br />
+                                <input
+                                    type="text"
+                                    id="name"
+                                    value={this.state.name}
+                                    /* onChange={this.handleChange.bind(this)} */
+                                    required
+                                    className="input"
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="email">Email:</label><br />
+                                <input
+                                    type="email"
+                                    id="email"
+                                    value={this.state.email}
+                                    /* onChange={this.handleChange.bind(this)} */
+                                    required
+                                    className="input"
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="message">Message:</label><br />
+                                <textarea
+                                    id="message"
+                                    value={this.state.message}
+                                    /* onChange={this.handleChange.bind(this)} */
+                                    required
+                                    className="message"
+                                />
+                            </div>
+                            <button className="submit" type="submit">{buttonText}</button>
+                        </form>      
+                    </div>
+                </div>
+            );
+        }
+  }
+
+  export default ContactForm;
